8f1f2efc78fdbbe997dd1b308ec52824f5ea1f10
Praneet <praneetquantumx@gmail.com>
2021-04-23 17:29:39 +0530

    Add: JSEngine connection

--- Playground/ContentView.swift
+++ Playground/ContentView.swift
@@ -9,6 +9,18 @@ import SwiftUI
 import CodeViewer
 import JavaScriptCore
 
+struct ConsoleView: View {
+    @Binding var result: String
+    var body: some View {
+        HStack{
+            VStack {
+                Text(result)
+            }.frame(height: 250)
+            Spacer()
+        }
+    }
+}
+
 struct EditorView: View {
     @Binding var jsCode: String
     var body: some View {
@@ -20,19 +32,27 @@ struct EditorView: View {
 
 struct ContentView: View {
     @State var jsCode: String = ""
+    @State var result: String = ""
+    @State var isResultShown: Bool = false
     var jsEngine: JSEngine = JSEngine.shared
     var body: some View {
         HStack {
             List {
                 Button(action: {
-                    print(jsEngine.run(source: jsCode))
+                    result = "\(jsEngine.run(source: jsCode))"
+                    isResultShown = true
                 }, label: {
                     Label("Run", systemImage: "play.fill")
                 })
             }
             .frame(width: 200)
             .listStyle(InsetGroupedListStyle())
-            EditorView(jsCode: $jsCode)
+            VStack {
+                EditorView(jsCode: $jsCode)
+                if isResultShown {
+                    ConsoleView(result: $result)
+                }
+            }
         }
     }
 }
